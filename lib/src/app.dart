99aaaca41b3cc61d6f976fc0030ec243338ec140
Andy Joseph <aj@droidian.com>
2024-11-22 16:43:50 -0500

    Saving

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,22 +4,39 @@ import 'background_view.dart';
 import 'top_level_coordinator.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App.local({super.key}) : _isLocal = true;
+  const App.remote({super.key}) : _isLocal = false;
+
+  final bool _isLocal;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    if (_isLocal) {
+      return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'ProntoGUI',
+        title: 'ProntoGUI Editor',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const WaitingForServerView(
-          operatingView: TopLevelCoordinator(
-            backgroundView: BackgroundView(),
+        home: const TopLevelCoordinator(
+          backgroundView: BackgroundView(),
+        ),
+      );
+    } else {
+      return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'ProntoGUI',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
           ),
-        ));
+          home: const WaitingForServerView(
+            operatingView: TopLevelCoordinator(
+              backgroundView: BackgroundView(),
+            ),
+          ));
+    }
   }
 }
