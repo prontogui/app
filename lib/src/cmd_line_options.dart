ecf48b9f0ce1bdca3dd4153b374e734c29334c7b
Andy Joseph <aj@droidian.com>
2025-03-20 09:20:37 -0400

    Some clean up of project files and updating copyright notices.

--- lib/src/cmd_line_options.dart
+++ lib/src/cmd_line_options.dart
@@ -1,3 +1,6 @@
+// Copyright 2025 ProntoGUI, LLC.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 import 'dart:io';
 import 'package:args/args.dart';
 import 'package:dartlib/dartlib.dart';
